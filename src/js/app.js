bcf90386caf264296abc72761cdf9b97d8be685d
Christoffer Alvenstrand <chippen_1989@hotmail.com>
2020-08-10 23:15:43 +0200

    added example code

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,18 @@
 import '../scss/app.scss';
 
 // Your JS Code goes here
+const btn = document.getElementById('menu-toggle');
+const cls = { open: 'open', close: 'close' };
+let btnClass = cls.open;
+
+btn.addEventListener('click', () => {
+  if (btn.classList.contains(cls.open)) {
+    btn.classList.remove(btnClass);
+    btnClass = cls.close;
+  } else if (btn.classList.contains(cls.close)) {
+    btn.classList.remove(btnClass);
+    btnClass = cls.open;
+  }
+
+  btn.classList.add(btnClass);
+});
