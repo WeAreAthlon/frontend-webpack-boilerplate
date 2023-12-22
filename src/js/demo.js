75a86354acc967597052a8089cf3646bccb0b652
Plamen Nikolov <plamen@nikolov.biz>
2023-12-22 11:10:06 +0200

    Fix demo.js demo message.

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,5 +1,4 @@
-const demo = () =>
-  'Webpack Boilerplate v5.17.1 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
+const demo = () => 'Webpack Boilerplate v5.17.1 - SASS/PostCSS, ES6/7, browser sync.';
 
 // eslint-disable-next-line no-console
 console.log(demo());
