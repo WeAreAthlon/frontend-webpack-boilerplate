2b0a417653f09c60f48b0b5a28b2de7c558a24a7
Dan Pudsey <dan.pudsey@newscientist.com>
2023-12-29 13:18:38 +0000

    Removed unneeded items and added favicon and fonts

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,4 +1,4 @@
-const demo = () => 'Webpack Boilerplate v5.17.1 - SASS/PostCSS, ES6/7, browser sync.';
+const demo = () => 'Your customer orders';
 
 // eslint-disable-next-line no-console
 console.log(demo());
